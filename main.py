76234d3d97e71b0d5228eb5459d5e3d30e875a70
Subrat Kumar Singh <85783015+subratcodes@users.noreply.github.com>
2022-10-03 02:53:58 +0000

    gonna check again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import arrays as easyProb
 from trees import BinaryTreees
 from linkedList import LinkedList
+from slidingWindow import SubArray_with_given_sum
 
 
 # Btree=BinaryTreees()
@@ -10,8 +11,12 @@ from linkedList import LinkedList
 # a=Btree.lookup(0)
 # print(a)
 
-test=LinkedList(2)
-a=test.insert(3)
-a=test.insert(4)
+# test=LinkedList(2)
+# a=test.insert(3)
+# a=test.insert(4)
+# print(a)
+
+# test.printElements()
+arr=[1,2,3,7,5]
+a=SubArray_with_given_sum(arr,len(arr),12)
 print(a)
-test.printElements()
--- slidingWindow.py
+++ slidingWindow.py
@@ -26,6 +26,29 @@ def MaxSumSubArray(arr,n,k):
 
 
 
-def SubArray_with_given_sum(arr,n,k):
+def SubArray_with_given_sum(arr,n,sum):
+  currentSum=arr[0]
+  i=0
+  j=1
+  result=[]
+  while(j<n and i<j):
+    if(j<n):
+      currentSum+=arr[j]
+      j+=1
+    if(currentSum>sum):
+      currentSum-=arr[i]
+      i+=1
+    if(currentSum==sum):
+      print('Found',i,j)
+      result.append(i)
+      result.append(j)
+      return result
+
+  result.append(-1)
+  return result 
+  
+  
+      
+   
   
   
